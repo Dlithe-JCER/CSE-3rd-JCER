f81c1c46bb7f7a9be013cd918e92a6f20c7c18b3
Dlithe-JCER <incubationjcer@gmail.com>
2025-12-09 14:26:49 +0530

    update

--- Module-7(OOP's)/SimpleInheritance.py
+++ Module-7(OOP's)/SimpleInheritance.py
@@ -1,1 +1,41 @@
-# Inheritance 
+# SECTION -B
+# # Simple Inheritance/Single Inheritance Example in Python
+# class ChatGPT3_5: 
+#     version = "3.5"
+#     year=2022
+#     @staticmethod
+#     def chat():
+#         return "Hello! I am ChatGPT version 3.5"
+#     @staticmethod
+#     def gptInfo():
+#         return f"ChatGPT version {ChatGPT3_5.version} released in {ChatGPT3_5.year}"
+# class ChatGPT4(ChatGPT3_5):
+#     version = "4.0"
+#     year=2024
+#     @staticmethod
+#     def gptInfo():
+#         return f"ChatGPT version {ChatGPT4.version} released in {ChatGPT4.year}"
+# c2=ChatGPT4()
+# print(c2.chat())  
+
+
+
+# SECTION -A
+# Simple Inheritance/Single Inheritance Example in Python
+class ChatGPT3_5:
+    version='3.5'
+    year=2022
+    @staticmethod
+    def chat():
+        return "Hello! I am ChatGPT version 3.5"
+    @staticmethod
+    def displayInfo():
+        return f"ChatGPT version {ChatGPT3_5.version} released in {ChatGPT3_5.year}"
+class ChatGPT4(ChatGPT3_5):
+    version='4.0'
+    year=2024
+    @staticmethod
+    def displayInfo():
+        return f"ChatGPT version {ChatGPT4.version} released in {ChatGPT4.year}"
+c1=ChatGPT4()
+print(c1.chat())
